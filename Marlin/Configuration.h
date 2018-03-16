5a89cea6ee1014e5e2fe6a3327d356ec72e9cb08
sidddy <mail@svenhenkel.de>
2018-03-16 09:03:35 +0100

    Properly set extrusion steps per mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -535,7 +535,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 418 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 455 }
 
 /**
  * Default Max Feed Rate (mm/s)
